94b9a12f1427a1079532eeed558b3d91b31d0341
idah <idah@moringaproject.com>
2022-11-12 01:09:27 +0300

    create a js folder and move quiz.js file

--- js/quiz.js
+++ js/quiz.js
@@ -5,10 +5,19 @@ function check() {
     var q3=document.quiz.q3.value;
     var q4=document.quiz.q4.value;
     var q5=document.quiz.q5.value;
+    var result=document.getElementById("result");
+    var quiz=document.getElementById("quiz");
+   
     if (q1=="Tags") {c++}
     if (q2=="Programming") {c++}
     if (q3=="Cascading style sheet") {c++}
     if (q4==".js") {c++}
     if (q5=="Function Literal") {c++}
-        document.write(c);
+    
+        
+    if (c<=3) {
+      result.textcontent="Your result is ${c}.You have performed poorly,please Retake!"
+    } else {
+      result.textcontent="Your result is ${c}.Well done! You have fairly passed!!" 
+    }  
 }
