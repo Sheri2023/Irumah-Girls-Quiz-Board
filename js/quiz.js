73ab0faf75fdbcd00144d876696d308bf9621d94
idah <idah@moringaproject.com>
2022-11-15 23:24:20 +0300

    add quiz Instructions

--- js/quiz.js
+++ js/quiz.js
@@ -1,23 +1,50 @@
-function check() {
-    var c=1;
-    var q1=document.quiz.q1.value;
-    var q2=document.quiz.q2.value;
-    var q3=document.quiz.q3.value;
-    var q4=document.quiz.q4.value;
-    var q5=document.quiz.q5.value;
-    var result=document.getElementById("result");
-    var quiz=document.getElementById("quiz");
-   
-    if (q1=="Tags") {c++}
-    if (q2=="Programming") {c++}
-    if (q3=="Cascading style sheet") {c++}
-    if (q4==".js") {c++}
-    if (q5=="Function Literal") {c++}
-    
-        
-    if (c<=3) {
-      result.textcontent="Your result is ${c}.You have performed poorly,please Retake!"
+var score = 0;
+var answer = ["A", "B", "C", "D"];
+var point = 5;
+
+function merit() {
+  var percentage = score / 25 * 100;
+  $("#percentage").text("Your percentage is " + percentage + "%");
+  if (percentage >= 80) {
+    $("#remark").text("You have Excellently passed");
+  } else if (percentage >= 50) {
+    $("#remark").text("You have fairly passed");
+  } else {
+    $("#remark").text("You've scored poorly. You need to retake the test");
+  }
+  $("button#submit").click(function(event) {
+    var q1 = $("input:radio[name=q1]:checked").val();
+    var q2 = $("input:radio[name=q2]:checked").val();
+    var q3 = $("input:radio[name=q3]:checked").val();
+    var q4 = $("input:radio[name=q4]:checked").val();
+    var q5 = $("input:radio[name=q5]:checked").val();
+    if (q1 === undefined || q2 === undefined || q3 === undefined || q4 === undefined || q5 === undefined) {
+      alert("answer all the questions");
     } else {
-      result.textcontent="Your result is ${c}.Well done! You have fairly passed!!" 
-    }  
-}
+      if (q1 === answer[1]) {
+        score += point;
+      }
+      if (q2 === answer[0]) {
+        score += point;
+      }
+      if (q3 === answer[1]) {
+        score += point;
+      }
+      if (q4 === answer[1]) {
+        score += point;
+      }
+      if (q5 === answer[2]) {
+        score += point;
+      }
+      $("#results").text("Your score is " + score + " ");
+    }
+    merit(score);
+    event.preventDefault();
+    $(".ques").hide();
+    $(".total").show();
+  });
+  $("#retake").click(function() {
+    $(".ques").show();
+    $(".total").hide()
+  });
+};
